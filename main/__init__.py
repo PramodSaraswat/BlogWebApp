9f06d68547b8c33acf03b3e7c86f29d88b0e8769
PramodSaraswat <8441918081pramod@gmail.com>
2020-10-16 17:20:07 +0530

    migrate database to postgre

--- main/__init__.py
+++ main/__init__.py
@@ -4,10 +4,12 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app=Flask(__name__)
 app.config.from_object(Config)
 db=SQLAlchemy(app)
+migrate=Migrate(app,db)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login'
--- main/config.py
+++ main/config.py
@@ -1,10 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY='e7b70374bb3711cc41519ddf94418bb5678bf'
-    SQLALCHEMY_DATABASE_URI='sqlite:///blogdata.db'
-    MAIL_SERVER='smtp.gmail.com'
-    MAIL_PORT=465
-    MAIL_USE_SSL=True
-    MAIL_USERNAME=os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
+	SECRET_KEY='e7b70374bb3711cc41519ddf94418bb5678bf'
+	SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'blogdata.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	MAIL_SERVER='smtp.gmail.com'
+	MAIL_PORT=465
+	MAIL_USE_SSL=True
+	MAIL_USERNAME=os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
